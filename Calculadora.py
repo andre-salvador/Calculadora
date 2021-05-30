8c0e83c8cbd66a6803f29dc578fa7396f8c1eb36
André Salvador <andrersalvador@hotmail.com>
2021-05-30 19:01:02 -0300

    apenas um formato de numero o float

--- Calculadora.py
+++ Calculadora.py
@@ -4,19 +4,19 @@ def calculos():
     calculadora = input('Qual conta você deseja fazer? ')
     if calculadora == 'soma' or calculadora == '+':
         resultado = n1 + n2
-        print(f'a soma de {n1} e {n2} é: {resultado}')
+        print(f'a soma de {n1} e {n2} é: {resultado:.4f}')
     elif calculadora == 'subtracao'or calculadora == '-':
         resultado = n1 - n2
-        print(f'a subtração de {n1} e {n2} é: {resultado}')
+        print(f'a subtração de {n1} e {n2} é: {resultado:.4f}')
     elif calculadora == 'multiplicacao'or calculadora == '*':
         resultado = n1 * n2
-        print(f'a multiplicacao de {n1} e {n2} é: {resultado}')
+        print(f'a multiplicacao de {n1} e {n2} é: {resultado:.4f}')
     elif calculadora == 'divisao'or calculadora == '/':
         resultado = n1 / n2
-        print(f'a divisao de {n1} e {n2} é: {resultado}')
+        print(f'a divisao de {n1} e {n2} é: {resultado:.4f}')
     else:
         resultado = n1 ** n2
-        print(f'a potencia de {n1} e {n2} é: {resultado}')
+        print(f'a potencia de {n1} e {n2} é: {resultado:.4f}')
     
     return resultado
 
@@ -24,17 +24,10 @@ print('Calculadora em Python')
 
 while True:
 
-    print(' inteiro \n quebrados')
-    estilo = input('Você quer realizar contas em qual estilo de numero? ')
-    n1 = int(input('Qual é o primeiro numero? '))
-    n2 = int(input('Qual é o segundo numero? '))
+    n1 = float(input('Qual é o primeiro numero? '))
+    n2 = float(input('Qual é o segundo numero? '))
 
-
-    if estilo == 'inteiro'or estilo == 'int':
-        calculos()
-
-    elif estilo == 'quebrados'or estilo == 'float':
-        calculos()
+    calculos()
 
     denovo = input('Deseja calcular novamente? S/N \n').upper()
 
